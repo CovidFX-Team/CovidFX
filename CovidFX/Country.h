20105414b9a287f706e0542a33dac3ec24da1d9e
Jack Wang <39379864+reachjw@users.noreply.github.com>
2023-12-01 22:57:18 -0800

    fixed country dropdown menu

--- CovidFX/Country.h
+++ CovidFX/Country.h
@@ -1,13 +1,33 @@
+// Header file for Country class
+
+#ifndef COUNTRY_H
+#define COUNTRY_H
+
+
 #include <string>
 
 using namespace std;
 
-class Country
-{
+class Country {
+private:
+    string countryName;
+    double deathsPerCaseScale; // for a rating, used by taking total deaths per million/total cases per million/total times 100
+    int GDP, totalCasesPerMillion, totalDeathsPerMillion;
+
 public:
-	string countryName;
-	double deathsPerCaseScale;//for a rating, used by taking total deaths per million/total cases per million/total times 100
-	int GDP, totalCasesPerMillion, totalDeathsPerMillion;
-	Country(string countryName, int GDP, int totalCasesPerMillion, int totalDeathsPerMillion);
-	
+    Country(string countryName, int GDP, int totalCasesPerMillion, int totalDeathsPerMillion) {
+        this->countryName = countryName;
+        this->GDP = GDP;
+        this->totalCasesPerMillion = totalCasesPerMillion;
+        this->totalDeathsPerMillion = totalDeathsPerMillion;
+        this->deathsPerCaseScale = (double)(totalDeathsPerMillion / totalCasesPerMillion) * 100;
+    }
+
+    string getName() {return countryName;}
+    int getGDP() {return GDP;}
+    int getCases() {return totalCasesPerMillion;}
+    int getDeaths() {return totalDeathsPerMillion;}
+    double getDeathsPerCase() {return deathsPerCaseScale;}
 };
+
+# endif // COUNTRY_H
